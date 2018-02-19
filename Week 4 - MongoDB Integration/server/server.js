40b90717ac7eedffab0beec3c595dc52e8801f61
Gaurav Singhal <singhal.gaurav93@gmail.com>
2018-02-19 14:46:22 +0530

    Add Post Todos route and Refactoring

--- Week 4 - MongoDB Integration/server/server.js
+++ Week 4 - MongoDB Integration/server/server.js
@@ -1,58 +1,30 @@
-const mongoose = require("mongoose");
+var express = require("express");
+var bodyParser = require("body-parser");
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+//Local imports
+var {mongoose} = require("./db/mongoose");
+var {Todo} = require("./models/Todo");
+var {User} = require("./models/User");
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean
-    },
-    completedAt: {
-        type: Number
-    }
+var app = express();
+
+//Middleware
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    var todo = new Todo({
+        text: request.body.text
+    });
+    todo.save().then((doc) => {
+        response.send(doc);
+    }, (err) => {
+        response.status(400).send(err);
+        console.log("Error occured while saving");
+    });
 });
 
-// var newTodo = new Todo({
-//     text: "Cook Dinner"
-// });
 
-// newTodo.save().then((doc) => {
-//     console.log("Saved ToDo", doc)
-// }, (err) => {
-//     console.log("Unable to save todo");
-// });
 
-// var otherTodo = new Todo({
-//     text: "         Print Journal Voucher       ",
-//     completed: false,
-//     completedAt: new Date().getTime()
-// });
-// otherTodo.save().then((doc) => {
-//     console.log(JSON.stringify(doc, undefined, 2));
-// }, (err) => {
-//     console.log("Unable to Solve", err);
-// });
-
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        minlength: 4,
-        trim: true,
-    },
-
-});
-var newUser = new User({
-    email: "krazy@me.com"
-});
-newUser.save().then((doc) => {
-    console.log("User saved", doc);
-}, (err) => {
-    console.log("Failed to insert user");
+app.listen(3000, ()=> {
+    console.log("Started on Port 3000");
 });
