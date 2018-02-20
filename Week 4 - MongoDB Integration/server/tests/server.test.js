232e0819e36f5e2b4a8643b385acea4d010c71d1
Gaurav Singhal <singhal.gaurav93@gmail.com>
2018-02-20 15:27:53 +0530

    Add tests of GET /todos

--- Week 4 - MongoDB Integration/server/tests/server.test.js
+++ Week 4 - MongoDB Integration/server/tests/server.test.js
@@ -4,8 +4,18 @@ const request = require("supertest");
 const {app} = require("./../server");
 const {Todo} = require("./../models/Todo");
 
+const todos = [{
+    text: "First test Todo"
+}, {
+    text: "Second test Todo"
+}, {
+    text: "Third test Todo"
+}];
+
 beforeEach((done) => {
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -23,8 +33,8 @@ describe('POST /todos', () => {
                 if(err)
                     return done(err);
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(1);
-                    expect(todos[0].text).toBe(text);
+                    expect(todos.length).toBe(4);
+                    expect(todos[3].text).toBe(text);
                     done();
                 }).catch((e) => done(e));
              });
@@ -40,9 +50,21 @@ describe('POST /todos', () => {
                     return done(err);
 
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(3);
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe("GET /todos", () => {
+    it("should get all todos", (done) => {
+        request(app)
+            .get("/todos")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(3);
+            })
+            .end(done);
     });
 });
